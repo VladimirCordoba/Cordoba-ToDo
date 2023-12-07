6d10f95b679b7fdf3f9f2689cc1af1be569353d9
vr <dimkar@inbox.lv>
2023-12-07 16:55:05 +0200

    версия без класса RECORD Следующее задание. Get ендпоинт показывает состояние таска - закрытый, открытый
                             Есть отдельный post ендпоинт /closeTask?taskId=X
    
                             Где через квери параметр можно подать id такскаего статус поменяется на закрытый

--- src/main/java/com/example/TodoMessageController.java
+++ src/main/java/com/example/TodoMessageController.java
@@ -2,13 +2,18 @@ package com.example;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @RestController
 @RequestMapping
 public class TodoMessageController {
-    private final static List<String > posts = new ArrayList<>();
+
+    private final static Map<String, String> posts = new HashMap<>();
+
+    String s;
 
    /* @GetMapping(path = "message")
     public List<String> taskMessage() {
@@ -28,21 +33,38 @@ public class TodoMessageController {
     return posts;
     }*/
 @PostMapping("/task/create")
-    public Object  addPost(@RequestBody String text){
-        posts.add(text);
-      return ("/index");
-}
-    @GetMapping(path = "task")
-    public List<String> taskMessage() {
+    public Object  addPost(@RequestParam String task1, @RequestParam String status){
 
+    s=status;
+        posts.put(task1, status);
+      /*return ("/index");*/
     return posts;
+    }
+
+    //тут мы будем ненять ключ - таск выполнен
+
+    @PostMapping("/closeTask")
+    public Object  changeStatus(@RequestParam String task1 /*@RequestParam String status*/){
+        posts.put(task1, s);
+        /*return ("/index");*/
+        return posts;
     }
 
 
 
+    @GetMapping(path = "task")
+    public String convertWithStream() {
+        String mapAsString = posts.keySet().stream()
+                .map(key -> key + ":" + posts.get(key))
+                .collect(Collectors.joining(", ", "{", "}"));
+        return mapAsString;
+    }
 
+    /*@GetMapping(path = "task")
+    public Map<String, String> taskMessage() {
 
-
+    return posts;
+    }*/
 
 
 
