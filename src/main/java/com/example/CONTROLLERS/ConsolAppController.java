0ce382bc958106ec362e31c156919d96a66761f4
vr <dimkar@inbox.lv>
2024-03-17 01:32:43 +0200

    first commit 11

--- src/main/java/com/example/CONTROLLERS/ConsolAppController.java
+++ src/main/java/com/example/CONTROLLERS/ConsolAppController.java
@@ -7,14 +7,14 @@ import com.example.Status;
 import com.example.models.Tasks;
 import com.example.repo.TasksRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.server.DelegatingServerHttpResponse;
+import org.springframework.instrument.classloading.LoadTimeWeaver;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 @RestController  // поменял @Controller на @RestController поскольку он возвращает JSON
 // @Controller
@@ -51,7 +51,38 @@ public  class ConsolAppController {
         tasksRepository.save(task);
 
     }
+    @PostMapping("consol/task/update/priority")
+    public void updateTasksConsol(@RequestParam String id1, String priority1, Model model) {
+     //  String priority = priority1;
+        // priority = null;
+        Priority  priority = null;
+       if (Objects.equals(priority1, "1")){
+            priority = Priority.HIGH;
+       }else if (Objects.equals(priority1, "2")){
+            priority = Priority.MEDIUM;
+        } else if (Objects.equals(priority1, "3")) {
+          priority = Priority.LOW;
+       }
 
+
+        int  intid = Integer.parseInt(id1); //преобразовываем строку в число.
+        Long id =(long)intid;
+
+        if(!tasksRepository.existsById(id)){
+            System.out.println("id does not exist");
+        }
+        Tasks tasks1 = tasksRepository.findById(id).orElseThrow();
+
+
+      //  model.addAttribute("listOfTasks", tasks1);
+      //  tasks1.setStatus(status);
+      //  tasks1.setTask(task);
+        tasks1.setPriority(priority);
+        tasksRepository.save(tasks1);
+
+        // return "redirect:/taskEdit";
+       // return "taskEdit";
+    }
 
 
 }
