88dfa95f9454ad131891456d777f7f4f5fcef712
vr <dimkar@inbox.lv>
2024-03-18 11:46:56 +0200

    status change is done

--- src/main/java/com/example/CONTROLLERS/ConsolAppController.java
+++ src/main/java/com/example/CONTROLLERS/ConsolAppController.java
@@ -7,6 +7,7 @@ import com.example.Status;
 import com.example.models.Tasks;
 import com.example.repo.TasksRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.DelegatingServerHttpResponse;
 import org.springframework.instrument.classloading.LoadTimeWeaver;
 import org.springframework.stereotype.Controller;
@@ -51,19 +52,11 @@ public  class ConsolAppController {
         tasksRepository.save(task);
 
     }
-    @PostMapping("consol/task/update/priority")
-    public void updateTasksConsol(@RequestParam String id1, String priority1, Model model) {
-     //  String priority = priority1;
-        // priority = null;
-        Priority  priority = null;
-       if (Objects.equals(priority1, "1")){
-            priority = Priority.HIGH;
-       }else if (Objects.equals(priority1, "2")){
-            priority = Priority.MEDIUM;
-        } else if (Objects.equals(priority1, "3")) {
-          priority = Priority.LOW;
-       }
 
+
+   @PostMapping ("consol/task/update/priority")
+  //  @PostMapping(consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public void updateTasksConsol( @RequestParam String id1, String priority1, Model model) {
 
         int  intid = Integer.parseInt(id1); //преобразовываем строку в число.
         Long id =(long)intid;
@@ -73,16 +66,22 @@ public  class ConsolAppController {
         }
         Tasks tasks1 = tasksRepository.findById(id).orElseThrow();
 
+        if (Objects.equals(priority1,"1")){
 
-      //  model.addAttribute("listOfTasks", tasks1);
-      //  tasks1.setStatus(status);
-      //  tasks1.setTask(task);
-        tasks1.setPriority(priority);
+            tasks1.setPriority(Priority.HIGH);
+
+        }else if (Objects.equals(priority1,"2")) {
+
+           tasks1.setPriority(Priority.MEDIUM);
+
+        }else
+        {
+            tasks1.setPriority(Priority.LOW);
+
+        }
         tasksRepository.save(tasks1);
 
-        // return "redirect:/taskEdit";
-       // return "taskEdit";
-    }
 
+    }
 
 }
